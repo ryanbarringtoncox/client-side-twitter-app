b954d5362770da70dc6db1c98f7e370d86a11d46
Ryan Cox <ryanbarrington@gmail.com>
2013-02-20 16:08:26 -0500

    prints tweets containing user input to console

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,11 +1,23 @@
 var main = function() {
   
   $(".input-button").click(function() {
-    console.log($(".input-text")[0].value);  
-    //console.log($(".input-text").value);
+    
+    //console.log($(".input-text")[0].value); 
+    
+    var terms = $(".input-text")[0].value;
+
+    //create ctwitter object
+    var twitter = new ctwitter.CTwitter();
+    
+    //start the stream of tweets
+    twitter.stream("statuses/filter", { lang:"en", track:[terms] }, function (stream) {
+      stream.on("data", function (tweet) {
+  
+        console.log(tweet.text);
+        
+      });    
+    });
   });
 };
 
-$(document).ready(function() {
-  main();
-});
+$(document).ready(main);
