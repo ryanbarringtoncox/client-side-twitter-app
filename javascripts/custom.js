84649b79a055e4bc3b1901a85221c4be190509c2
core.editor <ryanbarrington@gmail.com>
2013-02-21 17:10:01 -0500

    removes airport js, adds text animation.  click button to see it

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -2,8 +2,23 @@ var main = function () {
 
   $(".input-button").click(function () {
     
-    $("#results").airport([ 'does it work...', 'with whole long sentences?', '...and yet i wonder.' ]);
-        
+    //get x number of tweets and append to airport array...  count, wait
+    
+    //when you have x number then go to airport display
+    
+  	$("div#demo_step2").textAnimation({
+  	    mode:"step",
+  	    minsize:20,            //minimum font size[integer]
+  	    maxsize:60,            //maximum font size[integer]
+  	    upper:false,           //is upper step? [boolean]
+  	    fixed:"top",           //which fixed top or bottom ["top","bottom"]
+  	    stuff:2.2,             //font stuff quantity[float]
+  	    delay:200,             //delay for between charactors animation
+  	    interval:0,            //interval for between animation
+  	    duration:1000          //animation duration
+  	});    
+    
+            
     var terms = $(".input-text").val();
     
     //add function to split array based on spaces?
