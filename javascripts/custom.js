6b7af7c8504ec63a99c63290f1dab1ecdd8a66ff
core.editor <ryanbarrington@gmail.com>
2013-02-22 11:53:16 -0500

    slides up user-input div

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -20,23 +20,28 @@ var main = function () {
     twitter.stream("statuses/filter", { lang:"en", track:[terms] }, function (stream) {
       stream.on("data", function (tweet) {
         
-        //append tweet text in a div
+        $("#user-input").slideUp();
+        
+        //remove results div if exists
+        $("#tweet").remove();
+        
+        //append tweet text in a di
         $("#results").append("<div id='tweet'>" + tweet.text + "</div>");
          
         //console.log(tweet.text);
        
         //apply animation
-      $("#tweet").textAnimation({
-          mode:"step",
-          minsize:22,   //minimum font size[integer]
-          maxsize:22,   //maximum font size[integer]
-          upper:false,  //is upper step? [boolean]
-          fixed:"top",  //which fixed top or bottom ["top","bottom"]
-          stuff:2.2,    //font stuff quantity[float]
-          delay:20,     //delay for between charactors animation
-          interval:0,   //interval for between animation
-          duration:700  //animation duration
-        });     
+        $("#tweet").textAnimation({
+            mode:"step",
+            minsize:22,   //minimum font size[integer]
+            maxsize:22,   //maximum font size[integer]
+            upper:false,  //is upper step? [boolean]
+            fixed:"top",  //which fixed top or bottom ["top","bottom"]
+            stuff:2.2,    //font stuff quantity[float]
+            delay:20,     //delay for between charactors animation
+            interval:0,   //interval for between animation
+            duration:700  //animation duration
+          });     
         
         //wait for animation... the remove tweet div
         delayedRemoval();      
