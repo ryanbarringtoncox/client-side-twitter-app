88e3d823c8aa2e86479979490739b9b5d4135d63
core.editor <ryanbarrington@gmail.com>
2013-02-22 12:10:53 -0500

    splits longer tweet lines to console with line break

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,5 +1,5 @@
 function delayedRemoval() {
-  timeoutID = window.setTimeout(removeTweetDiv, 4000);
+  timeoutID = window.setTimeout(removeTweetDiv, 5000);
 }
 
 function removeTweetDiv() {
@@ -13,6 +13,9 @@ var main = function () {
     //get user input        
     var terms = $(".input-text").val()
     
+    //slide up user-input div
+    $("#user-input").slideUp();
+    
     //create ctwitter object
     var twitter = new ctwitter.CTwitter();
     
@@ -20,7 +23,17 @@ var main = function () {
     twitter.stream("statuses/filter", { lang:"en", track:[terms] }, function (stream) {
       stream.on("data", function (tweet) {
         
-        $("#user-input").slideUp();
+        var tweet_array = tweet.text.split(" ");
+        console.log(tweet_array);
+        if (tweet_array.length > 15) {
+          console.log("Need to add line break");
+          var slice1 = tweet_array.slice(0, 14);
+          var slice1 = slice1.join(" ");
+          var slice2 = tweet_array.slice(15, 99);
+          var slice2 = slice2.join(" ");
+          console.log(slice1 + "\n" + slice2);
+          tweet.text = slice1 + "<br>" + slice2;
+        }
         
         //remove results div if exists
         $("#tweet").remove();
