1751aea7d53d0131346c0c1ec68309c8054cf32a
core.editor <ryanbarrington@gmail.com>
2013-02-25 21:13:30 -0500

    simplifies js, style

--- javascripts/custom/custom.js
+++ javascripts/custom/custom.js
@@ -14,29 +14,13 @@ var main = function () {
     //start the stream of tweets
     twitter.stream("statuses/filter", { lang:"en", track:[terms] }, function (stream) {
       stream.on("data", function (tweet) {
-      	
-      	console.log("new tweet");
-        
-        var tweet_array = tweet.text.split(" ");
-        console.log(tweet_array);
-        if (tweet_array.length > 15) {
-          console.log("Need to add line break");
-          var slice1 = tweet_array.slice(0, 14);
-          var slice1 = slice1.join(" ");
-          var slice2 = tweet_array.slice(15, 99);
-          var slice2 = slice2.join(" ");
-          console.log(slice1 + "\n" + slice2);
-          tweet.text = slice1 + "<br>" + slice2;
-        }
         
         //remove results div if exists
         $("#tweet").remove();
         
         //append tweet text in a di
         $("#results").append("<div id='tweet'>" + tweet.text + "</div>");
-         
-        //console.log(tweet.text);
-       
+                
         //apply animation
         $("#tweet").textAnimation({
             mode:"highlight",
